cecb31fbdd5f62be65e528debe3723f45e5693e6
johanadi <johanadigi@gmail.com>
2020-07-15 15:53:36 -0500

    deleted extra

--- graphit.js
+++ graphit.js
@@ -22,10 +22,22 @@ async function getWidget() {
       text = text.replace("</p>", "")
       text = text.trim()
 
-      // numbers[0] will store current location,
-      // numbers[1] will store where we want to get to
-      let numbers = Number(text.split("/",2))
+      
     }
+    widgets[0].text = text
+    // numbers[0] will store current location,
+    // numbers[1] will store where we want to get to
+    // numbers[2] will store the max width or height
+    var numbers = Number(text.split("/",3))
+
+    // we want to access to see if we are considering the x or the y axis
+    // if x axis, then take the x of the widget and assign it to the "max"
+
+    // if y axis, then take the y axis of the widget and assignt it to the "max"
+
+    // the max should remain unchanged, numbers[0] and [1] will make a percentage to determine
+    //    the current x/y of the bar
+
     // hide tip and show text in sidebar
     tipElement.style.opacity = '0'
     widgetTextElement.value = text
