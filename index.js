cb89ee15fef09aaf6ef14751a9b0325a2a9fba79
DiGirolamo <Johana_Di_girolamo@Dell.com>
2020-07-14 19:24:27 -0500

    Submission

---
+++ index.js
@@ -0,0 +1,19 @@
+miro.onReady(() => {
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'Sticker to shapes',
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        positionPriority: 1,
+        onClick: async () => {
+
+          // Launch the side view
+          miro.board.ui.openLeftSidebar('sidebar.html')
+
+          // Show success message
+          miro.showNotification('Clicked!')
+        }
+      }
+    }
+  })
+})
