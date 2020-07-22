f6a1bae51fd995229d66167328b6abe395d5e760
johanadi <johanadigi@gmail.com>
2020-07-21 20:02:04 -0500

    attempt #505

--- checksize.js
+++ checksize.js
@@ -17,9 +17,11 @@ async function getSize() {
 	axisS.forEach((axis) => {
 		if(axis.checked) {
 			if(axis.value === "x") {
-				text = widgets[0].bounds.width
+				//text = widgets[0].bounds.width
+				text = widgets[0].x
 			} else {
-				text = widgets[0].bounds.height
+				//text = widgets[0].bounds.height
+				text = widgets[0].y
 			}
 		}
 	})
